19cc7ec227b038ba5516e4fc208f05e1e703f13c
ioilmio <illuminato.salvatore@gmail.com>
2020-04-22 00:31:20 +0200

    not returning yet

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,22 @@
-def grett
-  puts 'hello'
+def bubble_sort(arr)
+  yield(arr)
 end
 
-grett
+arr = [5, 4, 3, 2, 1]
+
+bubble_sort(arr) do
+  loop do
+    l = arr.length - 1 
+    count = 0
+    l.times do |x|
+      if arr[x] > arr[x + 1]
+        arr[x], arr[x + 1] = arr[x + 1], arr[x]
+        count += 1
+        print arr
+      end
+      break if count == arr.length
+    end
+  end
+  return
+end
+
