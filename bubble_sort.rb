7bcf197cdca89f5a0b1bfc05797e7159a92069cb
ioilmio <illuminato.salvatore@gmail.com>
2020-05-02 02:05:39 +0200

    bubble_sort_by calling with block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,8 +7,24 @@ def bubble_sort(arr)
     end
     counter += 1
   end
-  print arr
+  p arr
 end
 
-arr = [5, 4, 3, 2, 1]
+def bubble_sort_by(arr)
+  l = arr.length - 1
+  counter = 0
+  while counter < arr.length
+    l.times do |x|
+      yield (arr[x], arr[x + 1] = arr[x + 1], arr[x]) if arr[x].length < arr[x + 1].length
+    end
+    counter += 1
+  end
+  p arr
+end
+
+arr = [17, 12, 85, 42, 23]
 bubble_sort(arr)
+
+bubble_sort_by(%w[thanks for your review]) do |right, left|
+  left.length - right.length
+end
